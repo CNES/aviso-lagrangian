8514206c08717a53d5fd9d77807f22abbf56d6dc
Frédéric Briol <fbriol@gmail.com>
2020-02-01 10:50:19 +0100

    Simplification of the setup script

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 set -e
-${PREFIX}/bin/python setup.py setup --netcdf-includes=${PREFIX}/include --netcdf-libraries=${PREFIX}/lib --boost-mt=0
-${PREFIX}/bin/python setup.py config
-${PREFIX}/bin/python setup.py build
+CXXFLAGS="-I${PREFIX}/include" ${PREFIX}/bin/python setup.py build
 ${PREFIX}/bin/python setup.py install
--- setup.py
+++ setup.py
@@ -17,10 +17,8 @@ package using Distutils.
 #
 # You should have received a copy of GNU Lesser General Public License along
 # with lagrangian. If not, see <http://www.gnu.org/licenses/>.
-try:
-    import configparser
-except ImportError:
-    import ConfigParser as configparser
+import pathlib
+import distutils.command.build
 import distutils.command.config
 import distutils.command.sdist
 import distutils.dist
@@ -37,6 +35,8 @@ import subprocess
 import sysconfig
 import sys
 import Cython.Build
+
+WORKING_DIRECTORY = pathlib.Path(__file__).parent.absolute()
 
 
 class SDist(distutils.command.sdist.sdist):
@@ -103,247 +103,33 @@ class SDist(distutils.command.sdist.sdist):
                 self.patch_trace_cpp(item, dest)
 
 
-class SetupConfig(object):
+def sources():
     """
-    Package configuration
+    Returns the list of necessary files to build the extension
     """
-    CWD = os.path.dirname(os.path.abspath(__file__))
-
-    def __init__(self):
-        super(SetupConfig, self).__init__()
-        self.path = os.path.join(self.CWD, 'setup.cfg')
-        self.parser = configparser.ConfigParser()
-        self.parser.read(self.path)
-
-    @staticmethod
-    def parse_boolean(v):
-        """
-        Parse a boolean value from a string
-        """
-        boolean_states = {
-            '1': True,
-            'yes': True,
-            'true': True,
-            'on': True,
-            '0': False,
-            'no': False,
-            'false': False,
-            'off': False
-        }
-        if v.lower() not in boolean_states:
-            raise ValueError('Not a boolean: %s' % v)
-        return boolean_states[v.lower()]
-
-    def write(self):
-        """
-        Write the configuration file
-        """
-        with open(self.path, 'w') as handle:
-            self.parser.write(handle)
-
-    def get_build_ext(self):
-        """
-        Gets the parameters to build the extension
-        """
-        include_dirs, libraries, library_dirs = (None, None, None)
-        if self.parser.has_section('build_ext'):
-            if self.parser.has_option('build_ext', 'include_dirs'):
-                include_dirs = self.parser.get('build_ext', 'include_dirs')
-                include_dirs = include_dirs.split(':') \
-                    if include_dirs else None
-            if self.parser.has_option('build_ext', 'libraries'):
-                libraries = self.parser.get('build_ext', 'libraries')
-                libraries = libraries.split(' ') \
-                    if libraries else None
-            if self.parser.has_option('build_ext', 'library_dirs'):
-                library_dirs = self.parser.get('build_ext', 'library_dirs')
-                library_dirs = library_dirs.split(':') \
-                    if library_dirs else None
-
-        return include_dirs, libraries, library_dirs
-
-    def set_build_ext(self, include_dirs, libraries, library_dirs):
-        """
-        Sets the parameters to build the extension
-        """
-        if not self.parser.has_section('build_ext'):
-            self.parser.add_section('build_ext')
-        if library_dirs:
-            self.parser.set('build_ext', 'library_dirs',
-                            ":".join(library_dirs))
-        elif self.parser.has_option('build_ext', 'library_dirs'):
-            self.parser.remove_option('build_ext', 'library_dirs')
-        include_dirs += [
-            'src/include',
-            os.path.join(
-                os.path.abspath("third_party"), "netcdf-cxx4", "cxx4")
-        ]
-        try:
-            import numpy.distutils.misc_util as np
-            include_dirs += np.get_numpy_include_dirs()
-        except ImportError:
-            pass
-        self.parser.set('build_ext', 'include_dirs', ":".join(include_dirs))
-        self.parser.set('build_ext', 'libraries', " ".join(libraries))
-
-    @classmethod
-    def sources(cls):
-        """
-        Returns the list of necessary files to build the extension
-        """
-        result = []
-        cxx4 = os.path.join("third_party", "netcdf-cxx4", "cxx4")
-        for item in os.listdir(cxx4):
-            if item.startswith("nc") and item.endswith(".cpp"):
-                result.append(os.path.join(cxx4, item))
-        for root, dirs, files in os.walk(os.path.join(cls.CWD, 'src')):
-            if 'wrapper' in dirs:
-                dirs.remove('wrapper')
-            for item in files:
-                if item.endswith('.cpp'):
-                    path = os.path.join(root, item)
-                    result.append(os.path.relpath(path, cls.CWD))
-        return result
-
-    @classmethod
-    def get_build_directory(cls, dirname):
-        """
-        Returns the Python build directory name
-        """
-        return "{dirname}.{platform}-{version[0]}.{version[1]}".format(
-            dirname=dirname,
-            platform=sysconfig.get_platform(),
-            version=sys.version_info)
+    result = []
+    cxx4 = os.path.join("third_party", "netcdf-cxx4", "cxx4")
+    for item in os.listdir(cxx4):
+        if item.startswith("nc") and item.endswith(".cpp"):
+            result.append(os.path.join(cxx4, item))
+    for root, dirs, files in os.walk(os.path.join(WORKING_DIRECTORY, 'src')):
+        if 'wrapper' in dirs:
+            dirs.remove('wrapper')
+        for item in files:
+            if item.endswith('.cpp'):
+                path = os.path.join(root, item)
+                result.append(os.path.relpath(path, WORKING_DIRECTORY))
+    return result
 
 
-class Setup(setuptools.Command, SetupConfig):
+def get_build_directory(dirname):
     """
-    Configuration commands
+    Returns the Python build directory name
     """
-    description = "Configuration commands"
-    user_options = [
-        ('boost-includes=', None, "Location of boost C++ headers"),
-        ('boost-libraries=', None, "Location of boost C++ libraries"),
-        ('boost-mt=', None, "Set to True to use boost C++ libraries with "
-         "multithreading support enabled"),
-        ('netcdf-includes=', None, "Location of NetCDF headers"),
-        ('netcdf-libraries=', None, "Location of NetCDF libraries"),
-        ('udunits-includes=', None, "Location of UDUNITS-2 headers"),
-        ('udunits-librairies=', None, "Location of UDUNITS-2 libraries"),
-    ]
-
-    def __init__(self, *args, **kwargs):
-        # super does not work because the class "setuptools.Command"
-        # does not inherit of object.
-        setuptools.Command.__init__(self, *args, **kwargs)
-        SetupConfig.__init__(self)
-
-    def initialize_options(self):
-        """
-        Initialize options to default values.
-        """
-        self.boost_includes = None
-        self.boost_libraries = None
-        self.boost_mt = str(sys.platform == 'darwin')
-        self.netcdf_includes = None
-        self.netcdf_libraries = None
-        self.udunits_includes = None
-        self.udunits_librairies = None
-
-    def finalize_options(self):
-        """
-        Parse boolean values read from the command line.
-        """
-        self.boost_mt = self.parse_boolean(self.boost_mt)
-
-    def run(self):
-        """
-        Set the values set by the user to build the library
-        """
-        # Needed boost libraries
-        boost = ['boost_date_time']
-
-        include_dirs = []
-        library_dirs = []
-
-        if self.boost_libraries:
-            library_dirs.append(self.boost_libraries)
-        if self.netcdf_libraries:
-            library_dirs.append(self.netcdf_libraries)
-        if self.udunits_librairies:
-            library_dirs.append(self.udunits_librairies)
-
-        if self.boost_includes:
-            include_dirs.append(self.boost_includes)
-        if self.netcdf_includes:
-            include_dirs.append(self.netcdf_includes)
-        if self.udunits_includes:
-            include_dirs.append(self.udunits_includes)
-
-        libraries = ['udunits2', "netcdf"]
-        if self.boost_mt:
-            libraries += ["%s-mt" % item for item in boost]
-        else:
-            libraries += boost
-
-        self.set_build_ext(include_dirs, libraries, library_dirs)
-        self.write()
-
-
-class Config(distutils.command.config.config, SetupConfig):
-    """
-    Configuration commands
-    """
-
-    def __init__(self, *args, **kwargs):
-        # old class style
-        distutils.command.config.config.__init__(self, *args, **kwargs)
-        SetupConfig.__init__(self)
-
-    def run(self):
-        """
-        Verification of the development environment
-        """
-        include_dirs, libraries, library_dirs = self.get_build_ext()
-        # Under Ubuntu sysconfig.get_path('include') does not return the path
-        # to pyconfig.h
-        include_py = os.path.dirname(sysconfig.get_config_h_filename())
-        if include_dirs is None:
-            include_dirs += []
-        include_dirs.append(include_py)
-
-        for header in ['netcdf.h', 'udunits2.h']:
-            distutils.log.info('Checking for C header file %r' % header)
-            if not self.check_header(header, include_dirs=include_dirs):
-                raise distutils.errors.DistutilsPlatformError(
-                    'Cannot find %r header.' % header)
-
-        for header in [
-                'netcdf',
-                os.path.join('boost', 'date_time.hpp'),
-                os.path.join('boost', 'version.hpp')
-        ]:
-            distutils.log.info('Checking for C++ header file %r' % header)
-            # Work around a bug of the "check_header" that does not take into
-            # account the parameter "lang".
-            # if not self.check_header(header, lang='c++'):
-            if not self.try_cpp(
-                    body="/* No body */",
-                    headers=[header],
-                    include_dirs=include_dirs,
-                    lang='c++'):
-                raise distutils.errors.DistutilsPlatformError(
-                    'Cannot find %r header.' % header)
-
-        for library in libraries:
-            distutils.log.info('Checking for library %r' % library)
-            if not self.try_link(
-                    "int main (void) { }",
-                    libraries=[library],
-                    library_dirs=library_dirs,
-                    lang='c++'):
-                raise distutils.errors.DistutilsPlatformError(
-                    'Cannot find library %r.' % library)
+    return "{dirname}.{platform}-{version[0]}.{version[1]}".format(
+        dirname=dirname,
+        platform=sysconfig.get_platform(),
+        version=sys.version_info)
 
 
 CLASSIFIERS = [
@@ -373,23 +159,14 @@ EXTENSIONS = [
         name='lagrangian.core',
         language='c++',
         extra_compile_args=EXTRA_COMPILE_ARGS,
-        sources=[os.path.join("src", "wrapper", "core.pyx")] +
-        SetupConfig.sources(),
+        sources=[os.path.join("src", "wrapper", "core.pyx")] + sources(),
         extra_link_args=EXTRA_LINK_ARGS,
-        library_dirs=[
-            os.path.join('build', SetupConfig.get_build_directory('lib'))
-        ])
+        libraries=["netcdf", "udunits2", "boost_date_time"],
+        library_dirs=[os.path.join('build', get_build_directory('lib'))])
 ]
 
-# Create the default setup configuration
-SETUP = Setup(distutils.dist.Distribution())
-if not os.path.exists(SETUP.path):
-    SETUP.initialize_options()
-    SETUP.run()
-
-with open(
-        os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                     'README.md')) as stream:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       'README.md')) as stream:
     LONG_DESCRIPTION = "".join(stream.readlines())
 
 distutils.core.setup(
@@ -415,14 +192,10 @@ distutils.core.setup(
     ],
     url='https://bitbucket.org/cnes_aviso/lagrangian',
     classifiers=CLASSIFIERS,
-    cmdclass={
-        'setup': Setup,
-        'config': Config,
-        'sdist': SDist
-    },
+    cmdclass={'sdist': SDist},
     ext_modules=Cython.Build.cythonize(
         EXTENSIONS,
         include_path=[
-            os.path.join(SetupConfig.CWD, "src", "include"),
-            os.path.join(SetupConfig.CWD, "src", "wrapper")
+            os.path.join(WORKING_DIRECTORY, "src", "include"),
+            os.path.join(WORKING_DIRECTORY, "src", "wrapper")
         ]))
